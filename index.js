ee7b41c73b6ba07d5f4e490772558e5b5049e916
UhaiMong <uhaimarma2016@gmail.com>
2022-11-26 13:29:44 +0600

    seller data load

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ app.use(cors());
 app.use(express.json());
 
 
-// database connection
 
+// jwt token function
+
+// database connection
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cnhrqkg.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -24,35 +26,52 @@ async function run() {
         const addedProductsCollection = client.db('SmartResaleStall').collection('AllProducts');
 
         const brandsCollection = client.db('SmartResaleStall').collection('brands');
+        const usersCollection = client.db('SmartResaleStall').collection('users');
 
+        // Post operation
         app.post('/addedProducts', async (req, res) => {
             const products = req.body;
             const result = await addedProductsCollection.insertOne(products);
             res.send(result);
         });
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
 
-        // get operation 
+        // get operation
+        
         app.get('/addedProducts', async (req, res) => {
             const query = {};
             const products = await addedProductsCollection.find(query).toArray();
             res.send(products);
         });
+        app.get('/addedProducts', async (req, res) => {
+            const email = req.query.email;
+            const query = { sellerEmail: email };
+            const addedProducts = await addedProductsCollection.find(query).toArray();
+            res.send(addedProducts);
+        })
         app.get('/brands', async (req, res) => {
             const query = {};
             const brands = await brandsCollection.find(query).toArray();
             res.send(brands);
         });
+    
         app.get('/selectedBrand', async (req, res) => {
-            console.log(req.query.brand);
+            console.log(req.query.email);
             let query = {};
-            if (req.query.brand) {
+            if (req.query.email) {
                 query = {
-                    brand: req.query.brand
+                    brand: req.query.email
                 }
             }
             const selectedBrand = await addedProductsCollection.find(query).toArray();
             res.send(selectedBrand);
-        })
+        });
+
+        // Put operation
 
     }
     finally {
